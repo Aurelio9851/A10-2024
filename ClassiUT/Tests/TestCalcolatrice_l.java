c47245519e1074f6b0ae6f4a8db6fd992596cb8f
aurelio <aurelio.9581@gmail.com>
2024-01-17 15:48:20 +0100

    Test perdente

--- ClassiUT/Tests/TestCalcolatrice_l.java
+++ ClassiUT/Tests/TestCalcolatrice_l.java
@@ -5,40 +5,22 @@ import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-//scrivere al posto di "Classe" il nome della classe da testare
-public class TestClasse {
+public class TestCalcolatrice {
+    private static Calcolatrice cut;
+
     @BeforeClass
-    public static void setUpClass() {
-        // Eseguito una volta prima dell'inizio dei test nella classe
-        // Inizializza risorse condivise o esegui altre operazioni di setup
+    public static void setup() {
+        cut = new Calcolatrice();
     }
-              
-    @AfterClass
-    public static void tearDownClass() {
-        // Eseguito una volta alla fine di tutti i test nella classe
-        // Effettua la pulizia delle risorse condivise o esegui altre operazioni di teardown
-    }
-              
-    @Before
-    public void setUp() {
-        // Eseguito prima di ogni metodo di test
-        // Preparazione dei dati di input specifici per il test
-    }
-              
-    @After
-    public void tearDown() {
-        // Eseguito dopo ogni metodo di test
-        // Pulizia delle risorse o ripristino dello stato iniziale
-    }
-              
+
     @Test
-    public void testMetodo() {
-        // Preparazione dei dati di input
-        // Esegui il metodo da testare
-        // Verifica l'output o il comportamento atteso
-        // Utilizza assert per confrontare il risultato atteso con il risultato effettivo
+    public void testDivide() {
+        int result = cut.divide(10, 2);
+        assertEquals(5, result);
     }
-              
-    // Aggiungi altri metodi di test se necessario
+
+   /* @Test(expected = ArithmeticException.class)
+    public void testDivideByZero() {
+        cut.divide(10, 0);
+    }*/
 }
-        
